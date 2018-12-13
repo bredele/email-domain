3591e1ab074495ab20c300d25f1c275a399f3cdd
bredele <olivier.wietrich@gmail.com>
2018-12-13 10:03:43 -0700

    Add Readme.md

--- test/email-domain.js
+++ test/email-domain.js
@@ -10,12 +10,10 @@ test('should return null or undefined when no domain specified', assert => {
   assert.equal(email('hello@') == null, true)
 })
 
-
 test('should return null or undefined if no email or domain specified', assert => {
   assert.plan(1)
   assert.equal(email() == null, true)
 })
-
 
 test('should return null or undefined when email string does not contain a @ character', assert => {
   assert.plan(2)
@@ -35,5 +33,5 @@ test('should create email address from incomplete address and domain', assert =>
 
 test('should return null or undefined when the domain of the given email is not index of the given domain', assert => {
   assert.plan(1)
-  assert.equal(email('hello@go', 'gmail.com') == null, true)
+  assert.equal(email('hello@gm', 'gmail.com') == null, true)
 })
