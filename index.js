294e2e629a2751ff0a35d00e770e6905edf5b1c2
bredele <olivier.wietrich@gmail.com>
2018-12-13 09:53:45 -0700

    Return null or undefined if no arguments

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 
 /**
+ * Create email address from given email string and domain.
  *
+ * @param {String?} email (should contain at least a @)
+ * @param {String?} domain
+ * @return {String} or undefined/null
+ * @api public
  */
 
 module.exports = function (email, domain) {
-  
+
 }
--- test/email-domain.js
+++ test/email-domain.js
@@ -11,8 +11,19 @@ test('should return null or undefined when no domain specified', assert => {
 })
 
 
+test('should return null or undefined if no email or domain specified', assert => {
+  assert.plan(1)
+  assert.equal(email() == null, true)
+})
+
+
 test('should return null or undefined when email string does not contain a @ character', assert => {
   assert.plan(2)
   assert.equal(email('hello') == null, true)
   assert.equal(email('hello', 'gmail.com') == null, true)
+})
+
+test('should create email address from email string containing a @ character and domain', assert => {
+  assert.plan(1)
+  assert.equal(email('hello@', 'gmail.com'), 'hello@gmail.com')
 })
