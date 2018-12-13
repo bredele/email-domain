9a6a903de43b1f5ce6df69cbfe8bc34c4dbd5184
bredele <olivier.wietrich@gmail.com>
2018-12-13 09:51:09 -0700

    Return null or undefined when no domain specified

--- test/email-domain.js
+++ test/email-domain.js
@@ -9,3 +9,10 @@ test('should return null or undefined when no domain specified', assert => {
   assert.plan(1)
   assert.equal(email('hello@') == null, true)
 })
+
+
+test('should return null or undefined when email string does not contain a @ character', assert => {
+  assert.plan(2)
+  assert.equal(email('hello') == null, true)
+  assert.equal(email('hello', 'gmail.com') == null, true)
+})
